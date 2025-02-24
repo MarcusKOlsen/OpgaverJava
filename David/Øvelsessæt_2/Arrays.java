27e3dc75531acfcfdc7661ec39980697a993fd6d
djarahi <daviddjarahi@gmail.com>
2025-02-24 22:04:06 +0100

    Færdiggørelse af arrays fra sæt 1

--- David/Øvelsessæt_2/Arrays.java
+++ David/Øvelsessæt_2/Arrays.java
@@ -34,7 +34,6 @@ public class Arrays {
         }
         return counting;
     }
-
     public static String Stringify(int[] v){
         int i = 0;
         String text = "";
@@ -81,17 +80,158 @@ public class Arrays {
             }
             i++;
             }
-        int[] end = new int[divisor.size()];
-        for(int j = 0; j < divisor.size(); j++){
-            end[j] = divisor.get(j);
+            return listToArray(divisor);
         }
-            
-        return end; 
-    }
-
-    /* 
     public static int max(int[] v){
-        
+        int big = v[0];
+        for(int i = 1; i < v.length; i++){
+            if (v[i] > big){
+                big = v[i];
+            }
+        }
+        return big;
     } 
-    */
+    public static boolean subset(int[] v, int[] w){
+        int i = 0;
+        int j = 0;
+        while (i < v.length && j < v.length){
+                if (v[i] == w[j]){
+                    i++;
+                    j = 0;
+                }
+                else j++;
+
+            }
+        //Returns True if the loop stops because of the entire of v being gone through.
+        return i == v.length;
+        } 
+    public static int[] intersection(int[] v, int[] w){
+        ArrayList<Integer> intersec = new ArrayList<>();
+        for (int i = 0; i < v.length; i++){
+            for (int j = 0; j < w.length;j++ ){
+                if(v[i] == v[j]){
+                    intersec.add(v[i]);
+                    i++;
+                    j = 0;
+                }
+                
+                    j++;
+                
+            }
+        }
+        return listToArray(intersec);
     }
+    public static int firstPositionMax(int[] v){
+        int save = 0;
+        for (int i = 1; i < v.length; i++){
+            if(v[i] > v[save]){
+                save = i;
+            }
+        }
+        return save;
+    }
+    public static int lastPositionMax(int[] v){
+        int save = 0;
+        for (int i = 1; i < v.length; i++){
+            if(v[i] >= v[save]){
+                save = i;
+            }
+        }
+        return save;
+    }
+    public static int[] positionMax(int[] v){
+        int big = v[0];
+        ArrayList<Integer> positions = new ArrayList<>(0);
+        for (int i = 1; i < v.length; i++){
+            if (big == v[i]){
+                positions.add(i);
+            
+            }
+            else if(v[i] > big){
+                positions.clear();
+                positions.add(i);
+                big = v[i];
+            }
+            i++;
+        }
+        return listToArray(positions);
+
+    } 
+    public static void square(int[] v){
+        for(int i = 0; i < v.length; i++){
+            v[i] = v[i]*v[i];
+        }
+    }
+    public static void reverse(int[] v){
+        int temp;
+        for (int i = 0; i < v.length/2; i++){
+            temp = v[i];
+            v[i] = v[v.length-i];
+            v[v.length-i] = temp;
+        }
+    }
+    public static int[] join(int[] v, int[] w){
+        int[] joined = new int[v.length+w.length];
+        int i = 0;
+        while( i < v.length){
+           joined[i] = v[i];
+           i++; 
+        }
+        for(int j = 0; j < w.length;j++){
+            joined[i+j] = v[j]; 
+        }
+        return joined;
+    }
+    public static int[] sortedJoin(int[] v, int[] w){
+        int[] joined = new int[v.length+w.length];
+        int i = 0;
+        int j = 0;
+        while (i + j < v.length + w.length ){
+            if ( j == w.length){
+                joined[i+j] = v[i];
+                i++;
+            }
+            else if (i == v.length){
+                joined[i+j] = w[j];
+                j++;
+            }
+            else if (v[i] > w[j]){
+                joined[i+j] = w[j];
+                j++;
+            }
+            else{
+                joined[i+j] = v[i];
+                i++;
+            }
+    
+        }
+        return joined;
+        }
+    public static int[] shuffle(int[] v, int[]w){
+       
+        //Kræver de to lister er lige lange
+        int[] joined = new int[v.length*2];
+        for(int i = 0; i < v.length;i++ ){
+            joined[i*2] = v[i];
+            joined[i*2+1] = w[i];
+        }
+        return joined;
+    }
+    public static boolean isSorted(int[] v){
+        boolean current = true;
+        int i = 1;
+        while ( i < v.length && current){
+            current = v[i-1] <= v[i];
+            i++;
+        }
+        return current;
+    }
+    public static int[] listToArray(ArrayList<Integer> v){
+        int[] list = new int[v.size()];
+        for(int i = 0; i < v.size(); i++){
+            list[i] = v.get(i);
+        }   
+        return list; 
+    }
+    }
+    
